51e1fc3ea1630287e36dc29f26e014f2e0a75b20
Gayah Karmo <gayahkarmo@gmail.com>
2020-05-09 08:43:06 -0400

    new css

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,95 +1,77 @@
+var wins = 0;
+var losses = 0;
+var totalScore;
+var randomNum;
+var picOne;
+var picTwo;
+var picThree;
+var picFour;
 
-    var wins = 0;
-    var losses = 0;
-    var totalScore;
-    var randomNum;
-    var picOne;
-    var picTwo;
-    var picThree;
-    var picFour;
+//Generates a new random number when called
+function getRandomNum() {
+  randomNum = Math.floor(Math.random() * 102) + 19;
+  return $(".random-number").text(randomNum);
+}
+getRandomNum();
 
-    
-    //Generates a new random number when called
-    function getRandomNum () {
-        randomNum = Math.floor(Math.random() * 102) +19;
-        return $("#random-number").text(randomNum);
-    }
+//Reset total score to 0
+function resetScore() {
+  totalScore = 0;
+  return $(".total-score").text(totalScore);
+}
+resetScore();
+
+//Generates new button value
+function newButValue() {
+  picOne = Math.floor(Math.random() * 12) + 1;
+  picTwo = Math.floor(Math.random() * 12) + 1;
+  picThree = Math.floor(Math.random() * 12) + 1;
+  picFour = Math.floor(Math.random() * 12) + 1;
+}
+newButValue();
+
+//Updates game
+function gameRules() {
+  if (totalScore == randomNum) {
+    $(".outcome").text("YOU WIN!!");
     getRandomNum();
-
-    //Reset total score to 0 
-    function resetScore () {
-        totalScore = 0;
-        return $(".total-score").text(totalScore);
-    }
-    resetScore();
-
-    //Generates new button value
-    function newButValue () {
-        picOne = Math.floor(Math.random() * 12) + 1;
-        picTwo = Math.floor(Math.random() * 12) + 1;
-        picThree = Math.floor(Math.random() * 12) + 1;
-        picFour = Math.floor(Math.random() * 12) + 1;
-    }
     newButValue();
+    resetScore();
+    wins++;
+    $(".win-count").text(wins);
+  } else if (totalScore > randomNum) {
+    $(".outcome").text("YOU LOSE :(");
+    getRandomNum();
+    newButValue();
+    resetScore();
+    losses++;
+    $(".losses-count").text(losses);
+  }
+}
 
-    //Updates game
-    function gameRules () {
-        if (totalScore == randomNum) {
-          $(".outcome").text("YOU WIN!!");
-          getRandomNum();
-          newButValue();
-          resetScore();
-            wins++;
-          $(".win-count").text(wins);
-      } 
-      else if (totalScore > randomNum) {
-        $(".outcome").text("YOU LOSE :(");
-          getRandomNum();
-          newButValue();
-          resetScore();
-          losses++;
-          $(".losses-count").text(losses);
-      } 
-    }
+$(document).ready(function () {
+  // The following adds pic's number to total score
+  $("#pic1").on("click", function () {
+    totalScore = totalScore + picOne;
+    $(".total-score").text(totalScore);
+    gameRules();
+  });
 
-    
+  $("#pic2").on("click", function () {
+    totalScore = totalScore + picTwo;
+    $(".total-score").text(totalScore);
+    gameRules();
+  });
 
-    $(document).ready(function() {
+  $("#pic3").on("click", function () {
+    totalScore = totalScore + picThree;
+    $(".total-score").text(totalScore);
+    gameRules();
+  });
 
-    // The following adds pic's number to total score
-      $("#pic1").on("click", function() {
-
-        totalScore = totalScore + picOne;
-        $(".total-score").text(totalScore);
-        gameRules();
-    
-      });
-
-      $("#pic2").on("click", function() {
-        
-        totalScore = totalScore + picTwo;
-        $(".total-score").text(totalScore);
-        gameRules();
-
-
-      });
-
-      $("#pic3").on("click", function() {
-        
-        totalScore = totalScore + picThree;
-        $(".total-score").text(totalScore);
-        gameRules();
- 
-
-      });
-
-      $("#pic4").on("click", function() {
-        
-        totalScore = totalScore + picFour;
-        $(".total-score").text(totalScore);
-        gameRules();
-
-      });
-
-    });
-
+  $("#pic4").on("click", function () {
+    totalScore = totalScore + picFour;
+    $(".total-score").text(totalScore);
+    gameRules();
+  });
+});
